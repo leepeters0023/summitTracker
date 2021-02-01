a799423506366473ce159a61291f78c35d19c954
Lee Peters <lpeters0023@gmail.com>
2021-01-31 19:56:25 -0500

    correctly places point in polygon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
-import intersect from '@turf/intersect'
-import polygonize from '@turf/polygonize'
-import booleanPointInPolygon from '@turf/boolean-point-in-polygon'
+// import intersect from '@turf/intersect'
+// import polygonize from '@turf/polygonize'
+// import booleanPointInPolygon from '@turf/boolean-point-in-polygon'
+// import polygon from '@turf/helpers';
+// import point from '@turf/helpers';
 const polyline = require('@mapbox/polyline');
+let inside = require('point-in-polygon');
 
 const map = {
   polyline : "counGdw`}LAICIMEIDSpAwApEg@tB]`Am@nBy@|CcBrFa@xAmAxDgAtDc@bBs@zBmBzGyArEWj@U^a@XgAf@qH|Bk@Ne@HeA\\yDdAoBb@oAPg@LmA`@sBf@kAVmAJe@JeA`@c@LoCfAaFnAYb@SbCIh@Yb@i@d@S`@T\\DLBR{@jAm@`@[Z]`@g@z@Sl@Gp@[^c@NmAEi@Hg@LaCbAc@Z{@hA]Zc@V{@Xe@Fc@Lg@Fe@PaAv@Yd@_@d@_@VYLaA{@a@a@_B]c@Ig@?YLe@@gAi@e@Og@Ga@Ve@JkAQ[c@Gu@@s@Mq@Qq@[a@g@Kg@@e@Je@Bc@SSi@_@K_@WWa@Om@Bs@Ro@Jo@DGf@FRJTBb@LVNf@HnBl@d@@VQTAb@WNq@Rg@@m@No@DEf@Id@Sb@Ld@EXIf@Bd@?bAk@f@B^]G_@?gAIo@@w@^_@d@Cb@RXAh@e@rAMlB_@h@Bd@C^[Xi@`@[hAa@d@GdA?h@Jj@d@zAh@lBVf@@b@FdD`Ab@?nBWz@QnCu@`GmBlAWd@ElAWrHqBb@GtBe@rCi@hAc@rGmBlDqA`@S`@c@Xg@j@uA~AuFr@sCh@_BNm@p@_ClDcLdBkF^gBv@wCfByGt@gCd@yAD?LH",
@@ -14,16 +17,16 @@ const decode = (line) => {
   return polyline.decode(line); // can add a precision param here too, line, 6 for example
 }
 // everything must in long lat
-const poly1 = [-73.094722, 44.525068]
+const poly1 = [-73.094722, 45.525068];
 //decode(map.polyline)
-const poly2 = ([[
+const poly2 = [
   [-73.095087, 44.525099],
   [-73.093864, 44.525535],
   [-73.093660, 44.525007],
   [-73.095269, 44.524869],
-]])
+];
 
-console.log(poly1)
+console.log(inside(poly1, poly2));
 
 export const App = () => {
   return (
